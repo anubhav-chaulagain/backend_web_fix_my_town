6c03c698336c9b8c8e460fce73124c39906d6100
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2026-01-05 17:29:57 +0545

    user service

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,3 +4,5 @@ dotenv.config();
 export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 export const MONGODB_URI: string = process.env.MONGO_URI || "mongodb://localhost:27017/defaultdb"
 
+export const JWT_SECRET: string = 
+    process.env.JWT_SECRET || 'default'
