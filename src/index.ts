d203f542f311377862abb570109f726f3ecc39fa
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2026-01-05 17:04:48 +0545

    user type and create user and login user dto

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import bodyParser from "body-parser";
 import { connectDatabase } from "./database/mongodb";
 import { PORT } from "./config";
 
-//
 import dotenv from "dotenv";
 dotenv.config();
 
---
+++ src/types/user.type.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const UserSchema = z.object({
+    fullname: z.string().min(3).max(30),
+    email: z.email().min(5),
+    password: z.string().min(3).max(30),
+    number: z.string().min(10, "10 characters only!").max(10, "10 characters only!"),
+    role: z.enum(['user', 'admin']).default('user'),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
