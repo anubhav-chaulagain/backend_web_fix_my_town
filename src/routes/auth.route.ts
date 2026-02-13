92dcb5802188e6e7a8353885ebfb1f02c6170f55
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2026-02-13 14:37:12 +0545

    add routes for reqPass and resetPass

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -6,5 +6,11 @@ const router = Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+
+router.post(
+    '/request-password-reset',
+    authController.requestPasswordReset
+)
+router.post("/reset-password/:token", authController.resetPassword);
 export default router;
 
